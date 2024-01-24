277e9544d95f733d66fe3a65ac6363dc32c59682
Tarasha Bansal <tarashabansalwork@gmail.com>
2024-01-24 12:56:29 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tarasha Bansal on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
