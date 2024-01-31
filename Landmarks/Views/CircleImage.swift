f457d4843b829a4ef41ec8e555633a573936657b
Tarasha Bansal <tarashabansalwork@gmail.com>
2024-01-30 19:54:51 -0500

    Building lists and navigation complete

--- Landmarks/Views/CircleImage.swift
+++ Landmarks/Views/CircleImage.swift
@@ -1,24 +1,21 @@
-//
-//  CircleImage.swift
-//  Landmarks
-//
-//  Created by Tarasha Bansal on 1/24/24.
-//
-
 import SwiftUI
 
+
 struct CircleImage: View {
+    var image: Image
+
+
     var body: some View {
-        Image("turtlerock")
-            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-            .overlay{
-                Circle().stroke(.white,lineWidth: 4)
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
             }
-            .shadow(radius:7)
+            .shadow(radius: 7)
     }
-        
 }
 
+
 #Preview {
-    CircleImage()
+    CircleImage(image: Image("turtlerock"))
 }
